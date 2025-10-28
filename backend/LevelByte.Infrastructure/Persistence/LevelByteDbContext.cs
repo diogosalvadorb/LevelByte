fd2b92d80d7049c4aa5614c630b73792ac50fd91
diogosalvadorb <diogosalvadorb@gmail.com>
2025-10-28 10:04:53 -0300

    mapeamento entidade user e service geracao de token e hash de senha

--- backend/LevelByte.Infrastructure/Persistence/LevelByteDbContext.cs
+++ backend/LevelByte.Infrastructure/Persistence/LevelByteDbContext.cs
@@ -10,6 +10,7 @@ namespace LevelByte.Infrastructure.Persistence
 
         public DbSet<Article> Articles { get; set; }
         public DbSet<ArticleLevel> ArticleLevels { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
